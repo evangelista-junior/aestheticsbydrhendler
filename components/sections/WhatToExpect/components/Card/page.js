a37e034c0d25fcd2cff7c1a9a3bb6a8354f0afb1
Evangelista Junior <evan_jr@hotmail.com>
2025-09-18 22:02:23 +1000

    what to expect styling miscellaneous

--- components/sections/WhatToExpect/components/Card/page.js
+++ components/sections/WhatToExpect/components/Card/page.js
@@ -4,7 +4,7 @@ import Headings from "@/components/primary/Headings";
 
 export default function Card({ cardIcon, cardTitle, cardInfo }) {
   return (
-    <div className="flex flex-col h-full items-center gap-3 p-3 border border-gray-100 rounded-md shadow-xl text-gray-600">
+    <div className="flex flex-col h-full items-center justify-center gap-3 p-3 border border-gray-100 rounded-md shadow-xl text-gray-600">
       <div className="w-full flex items-center gap-3 bg-transparent">
         {cardIcon}
         <p className="text-xl font-medium tracking-wider">{cardTitle}</p>
