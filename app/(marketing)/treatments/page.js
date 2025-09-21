7e1e2aaf5e7960ddb8143950204cb83cae0ce2d9
Evangelista Junior <evan_jr@hotmail.com>
2025-09-21 17:41:48 +1000

    added a neutral background to the treatment page

--- app/(marketing)/treatments/page.js
+++ app/(marketing)/treatments/page.js
@@ -4,6 +4,8 @@ import { usePathname } from "next/navigation";
 import Card from "./components/Card/page";
 import { useEffect, useState } from "react";
 import Headings from "@/components/primary/Headings";
+import Image from "next/image";
+import neutralBackgorund from "@/public/images/neutral-background.png";
 
 export default function Treatments() {
   const [treatments, setTreatments] = useState([]);
@@ -31,18 +33,23 @@ export default function Treatments() {
   return (
     <section
       id="treatments"
-      className="p-6 xl:p-24 bg-white text-gray-600 flex flex-col items-center justify-center gap-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.1)]"
+      className="w-full p-6 xl:p-24 bg-white text-gray-600 flex flex-col items-center justify-center gap-9 drop-shadow-[0_0px_10px_rgba(0,0,0,0.1)]"
     >
+      <div className="absolute inset-0">
+        <Image
+          src={neutralBackgorund}
+          alt=""
+          fill
+          className="object-cove opacity-5"
+        />
+      </div>
       {isDedicatedPath && (
-        <Headings
-          headingType="h1"
-          className="font-extralight tracking-widest bg-primary-300 px-6 text-white -skew-x-12"
-        >
-          Treatments
+        <Headings headingType="h1" className="font-light tracking-widest">
+          Available treatments
         </Headings>
       )}
 
-      <div className="w-full grid 2xl:grid-cols-2 gap-6 xl:gap-12 ">
+      <div className="px-12 py-6 relative backdrop-blur-xl w-full grid 2xl:grid-cols-2 gap-6 xl:gap-12">
         {!!treatments &&
           treatments.map((treatment) => {
             return (
