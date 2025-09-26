af51d46a5047d89a4d53a6a9dfe386e63b42e4aa
Evangelista Junior <evan_jr@hotmail.com>
2025-09-27 08:47:38 +1000

    fix: useLoading.js -> useLoadingModal.js

--- store/useLoadingModal.js
+++ store/useLoadingModal.js
@@ -4,7 +4,4 @@ export const useLoading = create((set, get) => ({
   isLoading: false,
 
   setLoading: (value) => set({ isLoading: value }),
-  showLoading: () => {
-    console.log(get().isLoading + " vai se fudeee");
-  },
 }));
