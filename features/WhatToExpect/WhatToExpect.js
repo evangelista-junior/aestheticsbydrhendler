8ecdea38f9f5eed8536f317355197f2c39b4a84b
Evangelista Junior <evan_jr@hotmail.com>
2025-09-27 17:02:20 +1000

    fix: Benefits section reshaped*

--- features/WhatToExpect/WhatToExpect.js
+++ features/WhatToExpect/WhatToExpect.js
@@ -1,43 +1,43 @@
 import Image from "next/image";
 import benefitsImg from "@/public/images/benefits.png";
 import {
-  Activity,
-  Clipboard,
-  FileText,
-  Heart,
-  Shield,
-  Smile,
+  BookOpenCheck,
+  NotebookPen,
+  ScanFace,
+  ShieldAlert,
+  Sparkle,
+  Stethoscope,
 } from "lucide-react";
 import Card from "./components/Card/page";
 
 const BENEFITS = [
   {
-    icon: Shield,
+    icon: Stethoscope,
     title: "Medical Expertise & Safety",
     info: "Treatments performed with a comprehensive understanding of facial anatomy, always putting your safety at the forefront.",
   },
   {
-    icon: Clipboard,
+    icon: ScanFace,
     title: "Advanced Diagnostic Assessment",
     info: "A comprehensive facial evaluation that prioritizes facial harmony while honoring your unique features.",
   },
   {
-    icon: Activity,
+    icon: ShieldAlert,
     title: "Complication Management",
     info: "Peace of mind knowing any rare complications that may arise during treatment can be immediately addressed by a doctor.",
   },
   {
-    icon: Smile,
+    icon: Sparkle,
     title: "Natural-Looking Results",
     info: "Achieve subtle enhancements that respect your facial dynamics and natural expressions. A ‘fresh not frozen’ approach.",
   },
   {
-    icon: FileText,
+    icon: NotebookPen,
     title: "Personalised Treatment Plans",
     info: "Receive a comprehensive treatment strategy based on medical principles, not just aesthetic trends.",
   },
   {
-    icon: Heart,
+    icon: BookOpenCheck,
     title: "Patient Education & Empowerment",
     info: "We guide you through every step of your treatment journey, ensuring you understand your options and feel confident in every decision.",
   },
@@ -45,29 +45,29 @@ const BENEFITS = [
 
 export default function WhatToExpect() {
   return (
+    //flex justify-center items-center p-12
     <section
       id="expect"
-      className="bg-easyWhite text-gray-800 px-4 py-12 lg:px-8 flex flex-col items-center 2xl:flex-row gap-8"
+      className="w-full p-12 grid gap-6 2xl:grid-cols-[auto_1fr] justify-center items-center"
     >
-      <div className="w-full flex justify-center">
+      <div className="w-full 2xl:w-[90%] flex justify-center">
         <Image
           src={benefitsImg}
           alt="Benefits"
-          className="rounded-lg xl:w-2/3 max-h-[650px] object-cover shadow-2xl"
+          className="2xl:w-full max-h-[650px] object-cover shadow-2xl"
         />
       </div>
 
-      <div className="2xl:relative xl:-left-12 w-full p-4 justify-center xl:justify-start">
+      <div className="w-full 2xl:w-full">
         <ul className="grid sm:grid-cols-1 lg:grid-cols-2 gap-6">
           {BENEFITS.map(({ icon: Icon, title, info }) => (
             <li key={title}>
               <Card
                 cardIcon={
                   <Icon
-                    size={24}
                     aria-hidden="true"
                     focusable="false"
-                    className="p-0.5"
+                    className="h-6 w-6"
                   />
                 }
                 cardTitle={title}
--- features/WhatToExpect/components/Card/page.js
+++ features/WhatToExpect/components/Card/page.js
@@ -5,13 +5,13 @@ import Headings from "@/components/ui/Headings";
 export default function Card({ cardIcon, cardTitle, cardInfo }) {
   return (
     <div className="flex flex-col h-full items-center justify-center gap-3 p-3 border border-gray-100 rounded-md shadow-xl text-gray-600">
-      <div className="w-full flex items-center gap-3 bg-transparent">
-        {cardIcon}
-        <p className="text-xl font-medium tracking-wider">{cardTitle}</p>
-      </div>
+      <div className="w-full flex items-center gap-3 ">{cardIcon}</div>
 
       <div className="min-w-0">
-        <p className="font-light">{cardInfo}</p>
+        <p className="text-xl font-bold tracking-wide">{cardTitle}</p>
+        <p className="font-extralight tracking-wider text-gray-500">
+          {cardInfo}
+        </p>
       </div>
     </div>
   );
