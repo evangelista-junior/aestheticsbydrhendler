fbe6a986906d59735ddf47b6db62930573bbbc8a
Evangelista Junior <evan_jr@hotmail.com>
2025-09-29 21:58:18 +1000

    refactor: endpoint treatments(GET) is prepared to be filtered if fields param is sent on the url

--- app/api/v1/treatments/route.js
+++ app/api/v1/treatments/route.js
@@ -1,14 +1,37 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request) {
   try {
-    const treatments = await prisma.treatments.findMany();
+    const { searchParams } = new URL(request.url);
+    const fieldsOnUrl = searchParams.get("fields");
+
+    const fields = fieldsOnUrl?.split(`,`).filter((s) => s.trim());
+    const availableFields = Object.keys(prisma.treatments.fields).reduce(
+      (acc, af) => {
+        acc[af] = true;
+        return acc;
+      },
+      {}
+    );
+    const select = fields?.reduce((acc, f) => {
+      if (availableFields[f]) {
+        acc[f] = true;
+      }
+      return acc;
+    }, {});
+    const isFiltered = select && Object.keys(select).length > 0;
+
+    const treatments = await prisma.treatments.findMany({
+      ...(isFiltered && { select }),
+    });
+
     if (!treatments) {
       return NextResponse.json({ error: "Data not found!" }, { status: 404 });
     }
     return NextResponse.json({ treatments }, { status: 200 });
   } catch (err) {
+    console.log(err);
     return NextResponse.json({ error: err }, { status: 500 });
   }
 }
