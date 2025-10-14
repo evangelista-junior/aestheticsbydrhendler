7f270fb5d38edd8956d12e3f407cc872b842eeac
Evangelista Junior <evan_jr@hotmail.com>
2025-10-15 07:48:48 +1100

    style: Removed typing animation and changed title font

--- app/(marketing)/bookings/components/SelectInput/index.js
+++ app/(marketing)/bookings/components/SelectInput/index.js
@@ -13,15 +13,11 @@ export default function SelectInput({
       </label>
 
       <select
-        className={`mt-2 w-full border border-gray-300 px-3 py-2.5 outline-none focus:ring-2 focus:ring-easyDark/30
-                    ${
-                      errors
-                        ? "border-primary-300 placeholder-primary-300 text-primary-300"
-                        : "border-gray-300"
-                    }`}
+        className={`mt-2 w-full shadow px-1 py-2.5 outline-none focus:ring-2 focus:ring-easyDark/30 placeholder:text-red-900
+                    ${errors & "placeholder-primary-300 text-primary-300"}`}
         {...hookFormArgs}
       >
-        <option value="">{labelTitle}</option>
+        <option value={labelTitle}>{labelTitle}</option>
         {options.map((o, i) => (
           <option key={i} value={o}>
             {o}
