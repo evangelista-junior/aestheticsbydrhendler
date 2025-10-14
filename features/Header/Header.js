2302989ee502baad821be7dfc23c782a0e42dfa8
Evangelista Junior <evan_jr@hotmail.com>
2025-10-15 07:49:43 +1100

    style: refered to the last commit

--- features/Header/Header.js
+++ features/Header/Header.js
@@ -21,7 +21,7 @@ export default function Header() {
             className="w-full max-w-[600px]"
             alt=""
           />
-          <p className="w-full text-center tracking-widest text-md md:text-2xl font-extralight text-nowrap overflow-hidden typing">
+          <p className="w-full font-title text-center tracking-widest text-md md:text-2xl font-extralight text-nowrap overflow-hidden">
             Where medical expertise meets natural aesthetics
           </p>
 
