b960ad9c92faf140891f6d8e7434a7feabb598b8
Evangelista Junior <evan_jr@hotmail.com>
2025-11-05 17:31:55 -0300

    style (NavBar Button): change color to black

--- features/NavBar/components/NavHeaderButton/index.js
+++ features/NavBar/components/NavHeaderButton/index.js
@@ -1,6 +1,6 @@
 export default function NavHeaderButton({ children }) {
   return (
-    <p className="relative inline-block group cursor-pointer transition-all duration-300 tracking-wider uppercase">
+    <p className="relative inline-block group cursor-pointer transition-all duration-300 tracking-widest text-easyDark">
       <span
         className={`after:content-[''] after:absolute after:bottom-0 after:left-1/2 
           after:-translate-x-1/2 after:h-[2px] after:w-0 after:bg-primary-300 
