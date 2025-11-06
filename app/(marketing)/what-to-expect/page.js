432a55f4343b23f9436b516ffc2efdd7d2187f27
Evangelista Junior <evan_jr@hotmail.com>
2025-11-06 09:48:46 -0300

    amend (What to expect section): Remove junk import

--- app/(marketing)/what-to-expect/page.js
+++ app/(marketing)/what-to-expect/page.js
@@ -1,43 +1,28 @@
-import Image from "next/image";
-import benefitsImg from "@/public/images/benefits.png";
-import {
-  BookOpenCheck,
-  NotebookPen,
-  ScanFace,
-  ShieldAlert,
-  Sparkle,
-  Stethoscope,
-} from "lucide-react";
 import Card from "./components/Card/page";
+import Headings from "@/components/Headings";
 
-const BENEFITS = [
+const benefits = [
   {
-    icon: Stethoscope,
     title: "Medical Expertise & Safety",
     info: "Treatments performed with a comprehensive understanding of facial anatomy, always putting your safety at the forefront.",
   },
   {
-    icon: ScanFace,
     title: "Advanced Diagnostic Assessment",
     info: "A comprehensive facial evaluation that prioritizes facial harmony while honoring your unique features.",
   },
   {
-    icon: ShieldAlert,
     title: "Complication Management",
     info: "Peace of mind knowing any rare complications that may arise during treatment can be immediately addressed by a doctor.",
   },
   {
-    icon: Sparkle,
     title: "Natural Results",
     info: "Achieve subtle enhancements that respect your facial dynamics and natural expressions. A 'fresh not frozen' approach.",
   },
   {
-    icon: NotebookPen,
     title: "Personalised Treatment Plans",
     info: "Receive a comprehensive treatment strategy based on medical principles, not just aesthetic trends.",
   },
   {
-    icon: BookOpenCheck,
     title: "Patient Education & Empowerment",
     info: "We guide you through every step of your treatment journey, ensuring you understand your options and feel confident in every decision.",
   },
@@ -45,27 +30,17 @@ const BENEFITS = [
 
 export default function WhatToExpect() {
   return (
-    <section id="what-to-expect" className="w-4/5 relative mx-auto">
+    <section id="what-to-expect" className="w-4/5 relative mx-auto my-12">
+      <Headings
+        headingType="h2"
+        className="tracking-wider uppercase font-light mb-6 text-center"
+      >
+        Why Choose Us
+      </Headings>
       <div className="mt-12 h-full grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-3">
-        {BENEFITS.map(({ icon: Icon, title, info, gridClasses }) => (
+        {benefits.map(({ title, info }) => (
           <Card key={title} cardTitle={title} cardInfo={info} />
         ))}
-        {/* <div className="col-span-2 row-span-2 bg-red-200">1</div>
-        <div className="col-span-2 row-span-3 col-start-1 row-start-3 bg-red-200 shadow">
-          2
-        </div>
-        <div className="row-span-3 col-start-3 row-start-1 bg-red-200 shadow">
-          3
-        </div>
-        <div className="col-span-2 row-span-3 col-start-4 row-start-1 bg-red-200 shadow">
-          4
-        </div>
-        <div className="col-span-2 row-span-2 col-start-3 row-start-4 bg-red-200 shadow">
-          5
-        </div>
-        <div className="row-span-2 col-start-5 row-start-4 bg-red-200 shadow">
-          6
-        </div> */}
       </div>
     </section>
   );
